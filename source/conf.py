e97358a605cbfce107d390b2e9a46c4ee05afb77
Tobin Holliday <tobstobs@gmail.com>
2014-04-30 13:06:04 -0700

    MOTECH-803 Adding Intersphinx support
    Added intersphinx set up

--- source/conf.py
+++ source/conf.py
@@ -37,6 +37,13 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
+#intersphinx mapping
+intersphinx_mapping = {
+    'MOTECH-Platform-Campaigns' : ('http://motech-project.readthedocs.org/projects/motech-platform-campaigns/en/latest/', None),
+    'MOTECH-Platform-Medical-Records' : ('http://motech-project.readthedocs.org/projects/motech-platform-medical-records/en/latest/'),
+    'MOTECH-Platform-Communications' : ('http://motech-project.readthedocs.org/projects/motech-platform-communications/en/latest/'),
+    }
+
 #primary domain
 primary_domain='java'
 
